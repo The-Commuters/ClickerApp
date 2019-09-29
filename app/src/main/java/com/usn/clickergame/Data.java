a42f1497b4fa7b8d661d1657f9d57c487b0f3b78
Markus Fenes <markusfenes@gmail.com>
2019-09-29 23:48:18 +0200

    Removed Savestate

--- app/src/main/java/com/usn/clickergame/Data.java
+++ app/src/main/java/com/usn/clickergame/Data.java
@@ -7,29 +7,33 @@ import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class Data extends ViewModel {
-     private MutableLiveData<GameState> mGame = new MutableLiveData<>();
-     public LiveData<Integer> mPoints ;
     public MutableLiveData<GameState> mGame = new MutableLiveData<>();
 
-    private static final String NAME_KEY = "game";
-    private SavedStateHandle mState;
+    public LiveData<Integer> mPoints ;
 
-    public Data(SavedStateHandle savedStateHandle){
+   /* private static final String NAME_KEY = "game";
+    private SavedStateHandle mState;*/
+
+  /*  public Data(SavedStateHandle savedStateHandle){
          mState = savedStateHandle;
      }
 
+*/
 
-/*
+ /* public Data(GameState gameState){
+     this.mGame.setValue(gameState);
 
-    public LiveData<String> getGame() {
 
-         return mState.getLiveData(NAME_KEY, mGame);
+  }*/
+
+    public LiveData<GameState> getGame() {
+
+        return mGame;
 
     }
 
-    public void saveGame(GameState gameState){
+   /* public void saveGame(GameState gameState){
         mState.set(NAME_KEY, gameState);
+    }*/
     }
-    *
- */
 }
